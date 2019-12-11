9b058693a4353b2988dc33b7ed37eb661b781e24
James Buteera <butera1983@outlook.com>
2019-12-11 16:06:30 -0500

    basic functionality

---
+++ Max Allee.md
@@ -0,0 +1,3 @@
+Max Allee  
+Bio: A voracious learner who is passionate about technology and programming. Experienced in HTML5, JavaScript, Node.Js, the full MERN stack, and REACT Native  
+Repo URL: https://github.com/MaxwellAllee  
--- index.js
+++ index.js
@@ -5,11 +5,28 @@ const inquirer = require("inquirer");
 
 inquirer
   .prompt({
+    type: "input",
     message: "Enter your GitHub username",
     name: "username"
   })
-  .then(function({ username }) {
-    const queryUrl = `https://api.github.com/users/${username}/repos?per_page=100`;
+  .then(function(response) {
+    const queryUrl = `https://api.github.com/users/${response.username}`;
+    console.log(queryUrl)
+    axios.get(queryUrl)
+      .then((results)=>{
+        console.log(results.data)
+        const rez = results.data
+var profile = `${rez.name}  
+Bio: ${rez.bio}  
+Repo URL: ${rez.html_url}  
+`
+fs.writeFile(`${rez.name}.md`,profile, err=>{
+  if (err) console.log(err)
+  console.log("success")
+} )
+console.log(profile)
+      
+      })
   });
 
 
